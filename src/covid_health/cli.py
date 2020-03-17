d6860c7efe677a522571f7476377852ba4b9387d
ggbaro <gbaronegiacomo@gmail.com>
2020-03-17 13:26:13 +0100

    Added transcoding cli to main

--- src/covid_health/cli.py
+++ src/covid_health/cli.py
@@ -2,15 +2,18 @@ import click
 from .ita.cli import cli as ita
 from .prep_eurostat import cli as eurostat
 from .prep_worldometer import cli as worldometer
+from .transcoding.cli import cli as transcoding
 
 
 @click.group()
 def cli():
     pass
 
-cli.add_command(ita, name='ita')
-cli.add_command(eurostat, name='eurostat')
-cli.add_command(worldometer, name='worldometers')
+
+cli.add_command(ita, name="ita")
+cli.add_command(eurostat, name="eurostat")
+cli.add_command(worldometer, name="worldometers")
+cli.add_command(transcoding, name="transcoding-utils")
 
 
 def main():
