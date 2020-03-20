722f1556a1bb0f816cd9a198c0151104973611f5
ggbaro <46573388+ggbaro@users.noreply.github.com>
2020-03-20 16:33:58 +0100

    bugfix

--- src/covid_health/ita/prep_pcm_dpc.py
+++ src/covid_health/ita/prep_pcm_dpc.py
@@ -4,13 +4,15 @@ from ..transcoding.names.ita import col
 from ..transcoding.metadata import dtype
 
 
-dpc_covid_prov = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv"
-dpc_covid_reg = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+figures = {
+    "dpc-province": "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv",
+    "dpc-regions" "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+}
 
 
-def parse_covid_data(repo_url, dtype=dtype):
-    covid_stats = pd.read_csv(repo_url, parse_dates=["data"])
-    covid_stats = covid_stats.rename(columns=col_names)
+def parse_covid_data(figure="dpc-province", dtype=dtype, col=col):
+    covid_stats = pd.read_csv(figures[figure], parse_dates=["data"])
+    covid_stats = covid_stats.rename(columns=col)
     dtype = {k: v for k, v in dtype.items() if k in covid_stats.columns}
     covid_stats = covid_stats.astype(dtype)
     return covid_stats
