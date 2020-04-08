cd6f46953c5ee3a1cefa5cabcc5f4c843b35ea80
ggbaro <gbaronegiacomo@gmail.com>
2020-04-08 17:30:57 +0200

    new  datasets added

--- src/covid_health/ita/prep_istat.py
+++ src/covid_health/ita/prep_istat.py
@@ -1,22 +1,32 @@
-import click
 import os
-import pandas as pd
-import numpy as np
-from covid_health.transcoding.names.ita import col, var
-from covid_health.transcoding.metadata import dtype
-from covid_health.utils import download_and_parse_gzip_csv, download_and_parse_zip_csv
 
+import click
+import numpy as np
+import pandas as pd
+
+from covid_health.transcoding.metadata import dtype
+from covid_health.transcoding.names.ita import col, var
+from covid_health.utils import (
+    download_and_parse_gzip_csv,
+    download_and_parse_zip_csv,
+)
 
 url_istat = "http://dati.istat.it/DownloadFiles.aspx?&DatasetCode={}&Lang=IT"
 figures = {
     "death_causes": url_istat.format("DCIS_CMORTE1_RES"),
 }
 gz_figures = {
-    "2019_pop_regions": "http://demo.istat.it/pop2019/dati/regioni.gz",
-    "2018_pop_regions": "http://demo.istat.it/pop2018/dati/regioni.gz",
-    "2019_pop_province": "http://demo.istat.it/pop2019/dati/province.gz",
+    f"{year}_pop_{lb}": f"http://demo.istat.it/pop{year}/dati/{urlpiece}.gz"
+    for year in range(2012, 2020)
+    for lb, urlpiece in [
+        ("regions", "regioni"),
+        ("municipalities", "comuni"),
+        ("provinces", "province"),
+    ]
 }
-daily_deaths = "https://www.istat.it/it/files//2020/03/dati-comunali-giornalieri-1.zip"
+daily_deaths = (
+    "https://www.istat.it/it/files//2020/03/dati-comunali-giornalieri-1.zip"
+)
 col = col["istat"]
 dtype = dtype
 
@@ -30,25 +40,47 @@ def parse_istat_geodemo(figure):
     url = gz_figures[figure]
     df = pd.DataFrame(
         list(
-            download_and_parse_gzip_csv(url, delimiter=",", eurostat=False, skiprows=1)
+            download_and_parse_gzip_csv(
+                url, delimiter=",", eurostat=False, skiprows=1
+            )
         )
     )
-    df = df.filter(regex="Regione|Età|Provincia|Totale[\w ]+")  # noqa: W605
+    df = df.filter(
+        regex="Regione|Età|Provincia|Codice|Denominazione|Totale[\w ]+"
+    )  # noqa: W605
     df = df.rename(columns=col)
     df = df.loc[(df != "Totale").all(axis=1)]
     df = df.melt(
-        id_vars={"age", "region", "province"}.intersection(df.columns),
+        id_vars={
+            "age",
+            "region",
+            "province",
+            "municipality",
+            "prov_town_code",
+        }.intersection(df.columns),
         var_name="sex",
         value_name="population",
     )
     if time:
         df["time"] = time
+
+    if "age" in df.columns:
+        try:
+            df["age"] = df["age"].astype(int)
+            df = df[df["age"] <= 100]
+        except Exception:
+            df = df[df["age"].astype(int) <= 100]
+    
+    df["population"] = df["population"].astype(int)
+
     return df
 
 
 def parse_daily_deaths():
     df = pd.DataFrame(
-        download_and_parse_zip_csv(daily_deaths, encoding="latin-1", delimiter=",")
+        download_and_parse_zip_csv(
+            daily_deaths, encoding="latin-1", delimiter=","
+        )
     ).rename(columns=col)
     dtype = {
         "region_code": "category",
@@ -61,7 +93,10 @@ def parse_daily_deaths():
         "GE": str,
     }
     dtype.update(
-        {col: float for col in df.filter(regex="^MASCHI|FEMMINE|TOTALE").columns}
+        {
+            col: float
+            for col in df.filter(regex="^MASCHI|FEMMINE|TOTALE").columns
+        }
     )
     df = df.astype(dtype)
     df.info()
@@ -81,8 +116,8 @@ def parse_daily_deaths():
         "GE",
     ]
 
-    df = df.melt(id_vars=id_vars)
-    df["value"] = df["value"].replace(9999.0, np.nan)
+    df = df.melt(id_vars=id_vars, value_name="deaths")
+    df["deaths"] = df["deaths"].replace(9999.0, np.nan)
     df = df.dropna()
 
     df["sex"] = df["variable"].str[:-3].astype("category")
@@ -91,7 +126,12 @@ def parse_daily_deaths():
     df = df[df["sex"].values != "TOTALE"]
 
     df["timestamp"] = (
-        "20" + df["variable"].str[-2:] + "-" + df["GE"].str[:2] + "-" + df["GE"].str[2:]
+        "20"
+        + df["variable"].str[-2:]
+        + "-"
+        + df["GE"].str[:2]
+        + "-"
+        + df["GE"].str[2:]
     )
     df = df.loc[df.timestamp.values != "2015-02-29"]
     df = df.loc[df.timestamp.values != "2017-02-29"]
@@ -99,10 +139,14 @@ def parse_daily_deaths():
     df = df.loc[df.timestamp.values != "2019-02-29"]
     df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.floor("D")
 
-    df = df.drop(columns=["GE", "variable", "prov_town_code"])
+    df = df.drop(columns=["GE", "variable"])
 
-    # df = df.query("value < 9999 & value >= 0")
-    # missing = df.query("value == 9999")
+    # df = df.query("deaths < 9999 & deaths >= 0")
+    # missing = df.query("deaths == 9999")
+    if "prov_town_code" in df.columns:
+        df["prov_town_code"] = df["prov_town_code"].str.replace(
+            "^(0{1,})([1-9][0-9]+)$", r"\2"
+        )
 
     df.info()
     return df, (tot_reporting,)
@@ -111,7 +155,9 @@ def parse_daily_deaths():
 def parse_istat_dataset(df):
 
     if isinstance(df, str):
-        df = pd.read_csv(df, dtype={"Codice Comune": str}, parse_dates=["TIME"])
+        df = pd.read_csv(
+            df, dtype={"Codice Comune": str}, parse_dates=["TIME"]
+        )
 
     df = df.rename(columns=col)
 
---
+++ src/covid_health/transcoding/names/ecdc.py
@@ -0,0 +1,17 @@
+col = {
+    "ecdc": {
+        "year": "year",
+        "month": "month",
+        "day": "day",
+        "cases": "tot_n_cases",
+        "deaths": "n_deceased",
+        "countriesAndTerritories": "geo",
+        "geoId": "geo_id",
+        "countryterritoryCode": "country_code",
+        "popData2018": "population",
+    }
+}
+
+var = {
+    "ecdc": {},
+}
